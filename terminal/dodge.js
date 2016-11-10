e6912d1b5d525722d9f248dbb61181140f0f0a33
Aidan <aidanm@skilstak.sh>
2016-11-10 17:21:39 -0500

    save

--- terminal/dodge.js
+++ terminal/dodge.js
@@ -1,2 +1,14 @@
 var game = new Phaser.Game(320,568);
+game.state.add("Boot",Boot);
+game.state.start("Boot");
 
+
+
+
+class Boot {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+}
